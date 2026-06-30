199351a2d486bb0490492ad1358ede13e865ff4e
unknown <phutphanida>
2026-06-30 20:45:50 +0700

    folders prepared

--- lab02.dart
+++ lab02.dart
@@ -8,10 +8,26 @@ void main(){
   print(subjects);
   
   Map <String,int> studentScores = 
-  {'Mathematics': 90 , 'Art' : 75 ,'Economics' : 68 };
+  {'Mathematics': 90 , 'Art' : 75 ,'Economics' : 25 };
   print(studentScores.containsKey('Art'));
   
   studentScores['Music'] = 87 ;
   print(studentScores);
   
+  //พิมพ์วิชาและคะแนนทั้งหมด ที่มีตัว ‘a’ เป็นส่วนประกอบของชื่อวิชา
+  studentScores.forEach((subject, score) {
+  if (subject.toLowerCase().contains('a')) {
+    print('$subject : $score');
+  }
+});
+  //พิมพ์วิชาและคะแนนทั้งหมด ที่มีคะแนน มากกว่า 50 คะแนน
+  studentScores.forEach((subject,score) {
+    if (score > 50) {
+      print('$subject : $score');
+    }
+  });
+
 }
+  
+
+
